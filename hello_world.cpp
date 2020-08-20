67e83c5f0c1c4a3888de1b9d3c2111bd828f662e
wangyaojiu <69377161+wangyaojiu@users.noreply.github.com>
2020-08-20 11:40:48 +0800

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,18 @@
+/*
+* name:hello_world
+* Time:2020/08/20
+* Dev-cpp v5.11
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout<<"hello world"<<endl;
+	cout<<"welcome to c++"<<endl;
+	return 0;
+	
+		
+	
+}

a21c5fddcb17ed2d7f5e64b23ad1984503d65e7a
wangyaojiu <69377161+wangyaojiu@users.noreply.github.com>
2020-09-08 11:38:29 +0800

    9.5

---
+++ 2_352.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ 
+using namespace std;
+ int main(){
+ 	
+ 	int i;int j; int s;
+
+	 
+	 for(i=1;i<=9;i++){
+	     for(j=1;j<=i;j++) {
+		 
+            s=i*j;  	
+            cout<<i<<"*"<<j<<"="<<s<<"  ";
+	} cout<<endl;
+}
+           
+   return 0;
+    }
+
